5912789818502d13d929776e5ab5e65d1f2d643f
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2025-12-26 15:22:55 -0300

    Creación de Entidades: Usuario, Medicamentos y Solicitud

---
+++ subsidio.Dominio/Entities/SolicitudSubsidio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace subsidio.Dominio.Entities
+{
+    public class SolicitudSubsidio
+    {
+        public int Id { get; set; }
+
+        // --- PAREJA 1: El Usuario ---
+        public int SolicitanteId { get; set; } // <--- EL DNI (Para la Base de Datos)
+        public required Usuario Solicitante { get; set; } // <--- LA PERSONA (Para C#)
+
+        // --- PAREJA 2: El Medicamento ---
+        public int MedicamentoSolicitadoId { get; set; } // <--- EL CODIGO (Para la Base de Datos)
+        public required Medicamentos MedicamentoSolicitado { get; set; } // <--- LA CAJA (Para C#)
+
+        public DateTime FechaSolicitud { get; set; } = DateTime.UtcNow; // <--- Otro valor por defecto (pone la hora actual sola)
+        public Enums.EstadoSolicitud Estado { get; set; } = Enums.EstadoSolicitud.Pendiente;    // al hacer esto dejamos un valor por defecto
+    }
+}
