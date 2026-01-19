69d9eae6f54ac6d86ec3c4d5805bd04a49485271
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-01-19 12:47:43 -0300

    Implementación de control de estado

---
+++ subsidio.API/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace subsidio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet("ping")]
+
+        public IActionResult Ping()
+        {
+            return Ok(new { status = "Live" });
+        }
+    }
+}
